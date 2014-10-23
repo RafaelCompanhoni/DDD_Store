a769d9c1d30e5b5c701c744a1a36f4ee7bef3a0b
Rafael Companhoni <companhoni@gmail.com>
2014-10-22 22:25:41 -0200

    Acidentalmente untracked

---
+++ LuaBijoux.Web/Content/js/admin/frameworks/jquery/methods_pt.js
@@ -0,0 +1,38 @@
+﻿/*
+ * Localized default methods for the jQuery validation plugin.
+ * Locale: PT_BR
+ * Incluir aqui validações customizadas e adaptações de moeda, data, etc
+ */
+jQuery.extend(jQuery.validator.methods, {
+    date: function (value, element) {
+        return this.optional(element) || /^\d\d?\/\d\d?\/\d\d\d?\d?$/.test(value);
+    },
+    number: function (value, element) {
+        return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test(value);
+    },
+    
+});
+
+// Validação do CPF
+$.validator.addMethod("cpf", function (value, element, param) {
+    var i;
+    var l = '';
+    for (i = 0; i < value.length; i++) if (!isNaN(value.charAt(i))) l += value.charAt(i);
+    value = l;
+    if (value.length != 11) return false;
+    var c = value.substr(0, 9);
+    var dv = value.substr(9, 2);
+    var d1 = 0;
+    for (i = 0; i < 9; i++) d1 += c.charAt(i) * (10 - i);
+    if (d1 == 0) return false;
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9) d1 = 0;
+    if (dv.charAt(0) != d1) return false;
+    d1 *= 2;
+    for (i = 0; i < 9; i++) d1 += c.charAt(i) * (11 - i)
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9) d1 = 0;
+    if (dv.charAt(1) != d1) return false;
+    return true;
+});
+$.validator.unobtrusive.adapters.addBool("cpf");
---
+++ LuaBijoux.Web/Infrastructure/Mappers/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AutoMapper;
+using LuaBijoux.Core.DomainModels.Identity;
+using LuaBijoux.Web.Areas.Admin.Models;
+
+namespace LuaBijoux.Web.Infrastructure.Mappers
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ModelToDomainMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<CreateUserVM, AppUser>()
+                .ForMember(d => d.UserName, o => o.MapFrom(u => u.Email.ToLower()))
+                .ForMember(d => d.Email, o => o.MapFrom(u => u.Email.ToLower()))
+                .ForMember(d => d.Cpf, o => o.MapFrom(u => u.Cpf.Replace(".", "").Replace("-", "")))
+                .ForMember(d => d.Birthdate, o => o.MapFrom(u => Convert.ToDateTime(u.Birthdate)));
+
+            Mapper.CreateMap<EditUserVM, AppUser>()
+                .ForMember(d => d.UserName, o => o.MapFrom(u => u.Email.ToLower()))
+                .ForMember(d => d.Email, o => o.MapFrom(u => u.Email.ToLower()))
+                .ForMember(d => d.Cpf, o => o.MapFrom(u => u.Cpf.Replace(".", "").Replace("-", "")))
+                .ForMember(d => d.Birthdate, o => o.MapFrom(u => Convert.ToDateTime(u.Birthdate)));
+        }
+    }
+}
